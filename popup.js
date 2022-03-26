811e5ab17fcf93b70d097d59e50f5b3f0e9debc9
ngocanhnt269 <ngocanhnt269@gmail.com>
2022-03-26 11:38:14 -0700

    Anh added the function to show SOS button

---
+++ popup.js
@@ -0,0 +1,31 @@
+var currentUser;
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        currentUser = db.collection("users").doc(user.uid); //global
+        console.log(currentUser);
+        // the following functions are always called when someone is logged in
+        insertName();
+    } else {
+        // No user is signed in.
+        console.log("No user is signed in");
+        window.location.href = "login.html";
+    }
+});
+
+function insertName() {
+    // to check if the user is logged in:
+    currentUser.get().then(userDoc => {
+        //get the user name
+        var user_Name = userDoc.data().name;
+        console.log(user_Name);
+        $("#name-goes-here").text(user_Name); //jquery
+        // document.getElementByID("name-goes-here").innetText=user_Name;
+    })
+}
+//insertName();
+
+$(document).ready(function () {
+    $("#btnShow").click(function () {
+        $('#SampleModal').modal('show');
+    });
+});
