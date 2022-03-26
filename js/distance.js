bc6d0ef8ad9fa9805a11d630271c4cb55d8de503
ngocanhnt269 <ngocanhnt269@gmail.com>
2022-03-26 11:37:22 -0700

    Function to read the real Longitude and Latitude of meeting destination for Anh

--- js/distance.js
+++ js/distance.js
@@ -6,6 +6,8 @@ var geolocationID;
 var curLong;
 var curLat;
 var distanceToTheHost;
+var meet_long;
+var meet_lat
 
 function getLocation() {
     if (navigator.geolocation) {
@@ -20,6 +22,10 @@ function getLocation() {
 
 //getLocation();
 
+//Update to the database each 10 seccond
+
+myInterval = setInterval(getLocation, 10000);
+
 function showPosition(position) {
 
     curLong = position.coords.longitude;
@@ -28,11 +34,50 @@ function showPosition(position) {
     userLong.value = curLong;
 }
 
+function insertLongAndLatMeet() {
+    // to check if the user is logged in:
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            console.log(user.uid); // let me to know who is the user that logged in to get the UID
+            console.log(user.groupID);
+            userQuery = db.collection("users").doc(user.uid);
+
+            userQuery.get()
+                .then(userDoc => {
+                    groupID = userDoc.data().groupID;
+
+                    groupQuery = db.collection("groups").where("groupID", "==", groupID); // will to to the firestore and go to the document of the user
+                    groupQuery.get()
+                        .then(groupSnapshot => {
+                            groupSnapshot.forEach(group => {
+                                let group_lat = group.data().meetLat;
+                                let group_long = group.data().meetLong;
+                                setGlobalLatLong(group_lat, group_long);
+                                console.table({
+                                    meet_lat,
+                                    meet_long
+                                })
+                            })
+                        })
+                })
+        }
+    })
+}
+// insertLongAndLatMeet();
+
+function setGlobalLatLong(lat, long) {
+    meet_lat = lat;
+    meet_long = long;
+}
+
 
 function getDistanceToHost(position) {
     //Download meeting destination
-    var hostLatitude = 49.268568;
-    var hostLongtitude = -122.790790;
+    var hostLatitude = parseInt(meet_lat);
+    var hostLongtitude = parseInt(meet_long);
+
+    console.log(hostLatitude, hostLongtitude);
+    console.log(position)
     var dist = getDistanceInKm(position.coords.latitude, position.coords.longitude, hostLatitude, hostLongtitude);
     distance.innerHTML = "Distance to Host: " + dist.toPrecision(3) + " km";
     //upload to database
@@ -56,7 +101,6 @@ function getDistanceInKm(lat1, lon1, lat2, lon2) {
 function deg2rad(deg) {
     return deg * (Math.PI / 180)
 }
-
 
 function stopTracking() {
     if (navigator.geolocation) {
