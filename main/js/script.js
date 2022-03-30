85fde24b23554d42806a66dc63b2ad833ddd0d78
ngocanhnt269 <ngocanhnt269@gmail.com>
2022-03-29 21:31:57 -0700

    Anh added to code to change the isSafe status

--- main/js/script.js
+++ main/js/script.js
@@ -1,10 +1,30 @@
-$(".um-btn-sos").mouseup(function() {
+$(".um-btn-sos").mouseup(function () {
     clearTimeout(pressTimer)
     return false;
-}).mousedown(function() {
-    pressTimer = window.setTimeout(function() {
+}).mousedown(function () {
+    pressTimer = window.setTimeout(function () {
         // Add code here
         $("#umModal").modal('show');
     }, 2500)
     return false;
 });
+
+function saveUserStatus() {
+    document.getElementById('isSafe').value == true;
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if user is signed in:
+        if (user) {
+
+            //go to the correct user document by referencing to the user uid
+            currentUser = db.collection("users").doc(user.uid)
+            //write/update database
+            currentUser.update({
+                    isSafe: false,
+                })
+                .then(() => {
+                    console.log("Document successfully updated!");
+                    // saveStatus.innerHTML = "Successfully Upload To DataBase";
+                })
+        }
+    })
+}
